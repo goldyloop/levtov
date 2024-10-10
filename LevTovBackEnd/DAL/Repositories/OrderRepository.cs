41c29aed8cc109c5e3a6ceff217996bee98a6242
Rachel <r0504108994@gmail.com>
2024-10-10 13:07:36 +0300

    Rachely 7

---
+++ LevTovBackEnd/DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,57 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class OrderRepository : ICRUD<Order>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> CreateAsync(Order item)
+        {
+            _context.Orders.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.Orders.FindAsync(id);
+            if (product == null) return false;
+            _context.Orders.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Order>> ReadAllAsync()
+        {
+            //Console.WriteLine( "hello world");
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> ReadAsync(int id)
+        {
+            return await _context.Orders.FindAsync(id);
+        }
+
+        public async Task<Order> UpdateAsync(int id, Order item)
+        {
+            if (id != item.OrderId) return null;
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
+
+
